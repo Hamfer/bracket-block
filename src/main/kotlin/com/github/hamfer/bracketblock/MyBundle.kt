00706832509e490fe729539825c8c06bef796bcd
GitHub Action <action@github.com>
2023-09-14 06:57:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/hamfer/bracketblock/MyBundle.kt
+++ src/main/kotlin/com/github/hamfer/bracketblock/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hamfer.bracketblock
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/hamfer/bracketblock/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/hamfer/bracketblock/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.hamfer.bracketblock.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
